f8dbec0c387888f775d02ba66015fd9ae9a22944
Valtýr Örn Kjartansson <valtyr@gmail.com>
2018-09-18 18:52:12 +0000

    Test update

--- src/Views/Login/components/Welcome.js
+++ src/Views/Login/components/Welcome.js
@@ -9,33 +9,36 @@ const Welcome = ({navigation}) => (
     <StatusBar barStyle="dark-content" />
     <Particles>
       <View style={styles.content}>
-        <Text style={styles.title}>anorak</Text>
-        <Button title="Skrá inn" onPress={() => navigation.navigate('LoginKennitala')} />
+        <Text style={styles.title}>anorak.</Text>
+        <Button
+          title="Skrá inn"
+          onPress={() => navigation.navigate('LoginKennitala')}
+        />
       </View>
     </Particles>
   </View>
 );
 
 Welcome.navigationOptions = () => ({
-  header: Header,
+  header: Header
 });
 
 const styles = StyleSheet.create({
   root: {
     flex: 1,
     padding: 15,
-    backgroundColor: 'white',
+    backgroundColor: 'white'
   },
   content: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   title: {
     fontSize: 40,
     padding: 10,
     fontWeight: '800',
-    textAlign: 'center',
-  },
+    textAlign: 'center'
+  }
 });
 
 export default Welcome;
