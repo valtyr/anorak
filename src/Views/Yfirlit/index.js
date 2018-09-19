d8e5d1ecc19c0656b5b8ee6c3c81aa181d0e0f3f
Valtýr Örn Kjartansson <valtyr@gmail.com>
2018-09-19 10:00:38 +0000

    Revert "Update test"
    This reverts commit c2c981511164dcb4c75a7c9e033f5d082da3a101.

--- src/Views/Yfirlit/index.js
+++ src/Views/Yfirlit/index.js
@@ -12,7 +12,7 @@ import {Birthdays, Events, HeroImage, Posts} from './components';
 const Yfirlit = ({data, navigation}) => (
   <Screen title="Yfirlit" onRefresh={data.refetch} refreshing={data.loading}>
     <Hero />
-    <TitleBar title="Yfirlitssss" white />
+    <TitleBar title="Yfirlit" white />
     <HeroImage
       nextPeriod={
         data.currentUser &&
